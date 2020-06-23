edb24754da04f17a27241c05eb41319a8c2d32d5
Brad Beggs <brad.beggs@gmail.com>
2020-06-23 21:05:11 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
 # Add your variables here
+first_number = 1000234.1
+second_number = 30.1345
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
